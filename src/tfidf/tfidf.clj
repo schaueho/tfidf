bfdbaf38b3f52030506c3d9a034e35ee22678627
Holger Schauer <Holger.Schauer@gmx.de>
2015-07-14 20:36:41 +0200

    Add tf-idf computation per document.

--- src/tfidf/tfidf.clj
+++ src/tfidf/tfidf.clj
@@ -16,9 +16,7 @@
         terms (reduce conj #{}
                       (flatten (map keys alltfs)))
         count-docs-with-term (fn [term]
-                               (apply + (map #(if (get % term)
-                                                1
-                                                0)
+                               (apply + (map #(if (get % term) 1 0)
                                              alltfs)))
         doccount (count textseq)]
     (reduce (fn [resmap term]
@@ -27,4 +25,100 @@
                                   (+ (count-docs-with-term term) 1)))))
             {} terms)))
 
+(defn tfidf
+  "Returns a sequence of the tf-idf values for a sequence of texts (sequence of words)."
+  [textseq]
+  (let [alltfs (map tf textseq)
+        terms (reduce conj #{}
+                      (flatten (map keys alltfs)))
+        count-docs-with-term (fn [term]
+                               (apply + (map #(if (get % term)
+                                                1
+                                                0)
+                                             alltfs)))
+        doccount (count textseq)
+        idf (reduce (fn [resmap term]
+                      (assoc resmap term
+                             (Math/log (/ (+ doccount 1)
+                                          (+ (count-docs-with-term term) 1)))))
+                    {} terms)]
+    (map (fn [tfpdoc]
+           (map  (fn [term]
+                   (* (get tfpdoc term 0)
+                      (get idf term 0)))
+                 (keys tfpdoc)))
+           alltfs)))
 
+(defprotocol TfIdfProtocol
+  (add-doc [tfidfr doc])
+  (get-tfidf-for-doc [tfidfr docid])
+  (tfidf-doc-term [tfidfr docid term])
+  (term2pos [tfidfr term])
+  (docs-with-term [tfidfr term])
+  (doc2pos [tfidfr docid]))
+
+(defrecord TfIdf [docs maxdocid terms maxtermpos tfidfmat]
+  TfIdfProtocol
+  (add-doc [tfidfr doc]
+    (let [newdocid (inc maxdocid)
+          newtf (tf (:wordseq doc))
+          newdocs (assoc docs (:title doc) {:pos newdocid :tf newtf})
+          idf-helper (fn [newdocid docwithterms]
+                       (Math/log (/ (+ newdocid 1)
+                                    (+ docwithterms 1))))
+          [newterms newmaxtermpos]
+                  ;; collect all new terms, increase maxtermpos
+                  (loop [[term & restterms] (keys newtf)
+                          curmaxtermpos maxtermpos
+                          nterms terms]
+                     (if-let [termpos (get-in nterms [term :pos])]
+                       ;; term already known, update docswithterm (pos doesn't change)
+                       (let [lnterms (update-in nterms [term :docswithterm] inc)]
+                         (if (seq restterms)
+                           (recur restterms curmaxtermpos lnterms)
+                           [lnterms curmaxtermpos]))
+                       ;; term is new, hence docswithterm=1 and pos=max term pos+1
+                       (let [lnterms (assoc nterms term {:pos (inc curmaxtermpos)
+                                                         :docswithterm 1})]
+                         (if (seq restterms)
+                           (recur restterms (inc curmaxtermpos) lnterms)
+                           [lnterms  (inc curmaxtermpos)]))))
+          idfterms (reduce (fn [nterms term]  ;; update idf for all terms
+                             (assoc-in nterms [term :idf]
+                                       (idf-helper newdocid
+                                                   (get-in nterms [term :docswithterm]))))
+                           newterms (keys newterms))]
+      (->  tfidfr
+           (assoc :docs newdocs)
+           (assoc :maxdocid newdocid)
+           (assoc :terms idfterms)
+           (assoc :maxtermpos newmaxtermpos)
+           (assoc :tfidfmat
+             ;; calculate tf-idf for all terms, for all documents
+             (let [nterms (keys newterms)]
+               (map (fn [[doctitle docdata]]
+                      (map (fn [term]
+                             (* (get (:tf docdata) term 0)
+                                (get-in idfterms [term :idf] 0)))
+                           nterms))
+                    (seq newdocs)))))))
+  (get-tfidf-for-doc [tfidfr docid]
+    (when-let [docpos (doc2pos tfidfr docid)]
+      (nth (:tfidfmat tfidfr) docpos)))
+  (tfidf-doc-term [tfidfr docid term]
+    (when-let [doc-tfidf (get-tfidf-for-doc tfidfr docid)]
+      (when-let [termpos (term2pos tfidfr term)]
+        (nth doc-tfidf termpos))))
+  (term2pos [tfidfr term]
+    (dec (get-in tfidfr [:terms term :pos])))
+  (doc2pos [tfidfr doctitle]
+    (dec (get-in tfidfr [:docs doctitle :pos])))
+  (docs-with-term [tfidfr term]
+    (get-in tfidfr [:terms term :docwithterm] 0)))
+
+(defn tfidf-from-docs
+  "Returns a sequence of the tf-idf values for a sequence of documents."
+  [docseq]
+  (reduce (fn [tfidfr doc]
+            (add-doc tfidfr doc))
+          (->TfIdf {} 0 {} 0 []) docseq))
--- test/tfidf/tfidf_test.clj
+++ test/tfidf/tfidf_test.clj
@@ -8,12 +8,12 @@
 (def textcoll (list text1 text2 text3))
 
 (facts "Computing tf-idf"
-       (facts "Compute term-frequency"
-              (frequencies text1) => (contains {"is" 2 "This" 1}))
-       (facts "Compute normalized term-frequency for a text"
-              (tfidf/tf text1) => (contains {"is" 1 "This" 1/2}))
-       (facts "Compute inverted document frequencies for a collection"
-              (tfidf/idf textcoll) => (contains {"onli" 0.0,
+       (fact "Compute term-frequency"
+             (frequencies text1) => (contains {"is" 2 "This" 1}))
+       (fact "Compute normalized term-frequency for a text"
+             (tfidf/tf text1) => (contains {"is" 1 "This" 1/2}))
+       (fact "Compute inverted document frequencies for a collection"
+             (tfidf/idf textcoll) => (contains {"onli" 0.0,
                                                  "And" 0.6931471805599453,
                                                  "which" 0.2876820724517807,
                                                  "stupid" 0.6931471805599453,
@@ -30,5 +30,94 @@
                                                  "english" 0.0,
                                                  "silli" 0.6931471805599453,
                                                  "This" 0.6931471805599453,
-                                                 "test" 0.0})))
+                                                 "test" 0.0}))
+       (fact "Compute the sequence of tfidf values for a collection"
+             (tfidf/tfidf textcoll) => '((0.0 0.14384103622589034 0.34657359027997264 0.2876820724517807 0.0 0.0 0.34657359027997264 0.34657359027997264 0.0 0.34657359027997264 0.34657359027997264 0.0)
+                                         (0.0 0.14384103622589034 0.34657359027997264 0.34657359027997264 0.14384103622589034 0.0 0.0 0.0 0.0)
+                                         (0.0 0.34657359027997264 0.34657359027997264 0.0 0.0 0.34657359027997264 0.34657359027997264 0.0 0.0))))
 
+(def doc1 {:title "Doc1"
+           :wordseq '("This" "is" "a" "silli" "english" "text" "test" "which" "is"
+                      "onli" "here" "for" "test" "pars")})
+(def doc2 {:title "Doc2"
+           :wordseq '("Another" "stupid" "english" "text" "test" "which" "is" "onli" "for" "test")})
+(def doc3 {:title "Doc3"
+           :wordseq '("And" "just" "some" "other" "english" "text" "test" "onli" "for" "test")})
+(def doccoll (list doc1 doc2 doc3))
+
+(facts "Compute the tf-idf matrix for a document collection"
+       (let [tfidfr (tfidf/->TfIdf {} 0 {} 0 [])]
+         (fact "We can add documents incrementally"
+               (tfidf/add-doc tfidfr doc1) => (contains
+                                               {:docs
+                                                (contains
+                                                 {"Doc1"
+                                                  (contains {:pos 1
+                                                             :tf (contains {"onli" 1/2, "which" 1/2, "pars" 1/2,
+                                                                            "is" 1, "for" 1/2, "text" 1/2,
+                                                                            "a" 1/2, "here" 1/2, "english" 1/2,
+                                                                            "silli" 1/2, "This" 1/2, "test" 1})})})
+                                               :maxdocid 1
+                                               :terms (contains {"onli" {:pos 1, :docswithterm 1, :idf 0.0},
+                                                                 "which" {:pos 2, :docswithterm 1, :idf 0.0},
+                                                                 "pars" {:pos 3, :docswithterm 1, :idf 0.0},
+                                                                 "is" {:pos 4, :docswithterm 1, :idf 0.0},
+                                                                 "for" {:pos 5, :docswithterm 1, :idf 0.0},
+                                                                 "text" {:pos 6, :docswithterm 1, :idf 0.0},
+                                                                 "a" {:pos 7, :docswithterm 1, :idf 0.0},
+                                                                 "here" {:pos 8, :docswithterm 1, :idf 0.0},
+                                                                 "english" {:pos 9, :docswithterm 1, :idf 0.0},
+                                                                 "silli" {:pos 10, :docswithterm 1, :idf 0.0},
+                                                                 "This" {:pos 11, :docswithterm 1, :idf 0.0},
+                                                                 "test" {:pos 12, :docswithterm 1, :idf 0.0}})
+                                                :maxtermpos 12,
+                                                :tfidfmat '((0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0))}))
+         (fact "Compute tfidf for more documents"
+               (-> tfidfr
+                   (tfidf/add-doc doc1)
+                   (tfidf/add-doc doc2)
+                   (tfidf/add-doc doc3)) => (contains
+                                               {:docs
+                                                (contains
+                                                 {"Doc1"
+                                                  (contains {:pos 1
+                                                             :tf (contains {"onli" 1/2, "which" 1/2, "pars" 1/2,
+                                                                            "is" 1, "for" 1/2, "text" 1/2,
+                                                                            "a" 1/2, "here" 1/2, "english" 1/2,
+                                                                            "silli" 1/2, "This" 1/2, "test" 1})})})
+                                               :maxdocid 3
+                                               :terms (contains {"onli" {:pos 1, :docswithterm 3, :idf 0.0},
+                                                                 "And" {:pos 15, :docswithterm 1,
+                                                                        :idf 0.6931471805599453},
+                                                                 "which" {:pos 2, :docswithterm 2,
+                                                                          :idf 0.2876820724517807},
+                                                                 "stupid" {:pos 13, :docswithterm 1,
+                                                                           :idf 0.6931471805599453},
+                                                                 "pars" {:pos 3, :docswithterm 1,
+                                                                         :idf 0.6931471805599453},
+                                                                 "Another" {:pos 14, :docswithterm 1,
+                                                                            :idf 0.6931471805599453},
+                                                                 "is" {:pos 4, :docswithterm 2,
+                                                                       :idf 0.2876820724517807},
+                                                                 "just" {:pos 16, :docswithterm 1,
+                                                                         :idf 0.6931471805599453},
+                                                                 "for" {:pos 5, :docswithterm 3, :idf 0.0},
+                                                                 "text" {:pos 6, :docswithterm 3, :idf 0.0},
+                                                                 "a" {:pos 7, :docswithterm 1,
+                                                                      :idf 0.6931471805599453},
+                                                                 "here" {:pos 8, :docswithterm 1,
+                                                                         :idf 0.6931471805599453},
+                                                                 "other" {:pos 17, :docswithterm 1,
+                                                                          :idf 0.6931471805599453},
+                                                                 "some" {:pos 18, :docswithterm 1,
+                                                                         :idf 0.6931471805599453},
+                                                                 "english" {:pos 9, :docswithterm 3, :idf 0.0},
+                                                                 "silli" {:pos 10, :docswithterm 1,
+                                                                          :idf 0.6931471805599453},
+                                                                 "This" {:pos 11, :docswithterm 1,
+                                                                         :idf 0.6931471805599453},
+                                                                 "test" {:pos 12, :docswithterm 3, :idf 0.0}})
+                                                :maxtermpos 18,
+                                                :tfidfmat '((0.0 0.0 0.14384103622589034 0.0 0.34657359027997264 0.0 0.2876820724517807 0.0 0.0 0.0 0.34657359027997264 0.34657359027997264 0.0 0.0 0.0 0.34657359027997264 0.34657359027997264 0.0)
+                                                            (0.0 0.0 0.14384103622589034 0.34657359027997264 0.0 0.34657359027997264 0.14384103622589034 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0)
+                                                            (0.0 0.34657359027997264 0.0 0.0 0.0 0.0 0.0 0.34657359027997264 0.0 0.0 0.0 0.0 0.34657359027997264 0.34657359027997264 0.0 0.0 0.0 0.0))}))))
