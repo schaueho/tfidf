9355bc32e406ac0b2513933914147c86a507fa13
Holger Schauer <Holger.Schauer@gmx.de>
2017-11-18 18:47:54 +0100

    Update version number to 0.1.0.

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject de.find-method/tfidf "0.1.0-SNAPSHOT"
+(defproject de.find-method/tfidf "0.1.0"
   :description "Compute tfidf"
+  :url "http://github.com/schaueho/tfidf"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]]
   :profiles
-  {:dev {:dependencies [[midje "1.8.2"]
-                        [clojure-complete "0.2.4"]]}})
+  {:dev {:dependencies [[midje "1.8.2"]]}})
